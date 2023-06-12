362788220fa7d911be99c5d64738e130e7e42388
handongqingxue <1417140290@qq.com>
2023-06-12 11:28:54 +0800

    1

--- src/main/java/com/batchMesComDKListener/task/KeepWatchTask.java
+++ src/main/java/com/batchMesComDKListener/task/KeepWatchTask.java
@@ -1,5 +1,12 @@
 package com.batchMesComDKListener.task;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.json.JSONObject;
+
 import com.batchMesComDKListener.util.*;
 
 public class KeepWatchTask extends Thread {
@@ -35,9 +42,13 @@ public class KeepWatchTask extends Thread {
 					break;
 				checked=false;
 				Thread.sleep(3000);
-				APIUtil.keepWatchOnWorkOrder();
+				JSONObject kwowoJO = APIUtil.keepWatchOnWorkOrder();
 				//APIUtil.keepWatchOnWorkOrderTest();
-				System.out.println("巡回工单状态........");
+				System.out.println("巡回工单状态........"+kwowoJO.getBoolean("success"));
+				if(!kwowoJO.getBoolean("success")) {
+					System.out.println("ooooooooooooooo");
+					runBatFile("D:/apache-tomcat-8.5.57/bin/startup.bat");
+				}
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -45,4 +56,31 @@ public class KeepWatchTask extends Thread {
 		}
 	}
 	
+	public void runBatFile(String fileUrl) {
+		StringBuilder sb = new StringBuilder();
+	    try {
+	        Process child = Runtime.getRuntime().exec(fileUrl);
+	        InputStream in = child.getInputStream();
+	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+	        String line;
+	        while ((line = bufferedReader.readLine()) != null) {
+	        	System.out.println(line);
+	            sb.append(line + "\n");
+	        }
+	        in.close();
+	        try {
+	            child.waitFor();
+	            System.out.println("call cmd process finished");
+	        } catch (InterruptedException e) {
+	        	System.out.println("faild to call cmd process cmd because " + e.getMessage());
+	        }
+	    } catch (IOException e) {
+	    	System.out.println(e.getMessage());
+	    }
+	}
+	
+	public static void main(String[] args) {
+		KeepWatchTask k=new KeepWatchTask();
+		k.runBatFile("D:/apache-tomcat-8.5.57/bin/startup.bat");
+	}
 }
--- src/main/java/com/batchMesComDKListener/util/APIUtil.java
+++ src/main/java/com/batchMesComDKListener/util/APIUtil.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.*;
 import java.util.Map.*;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class APIUtil {
@@ -20,46 +24,54 @@ public class APIUtil {
 	//https://www.cnblogs.com/aeolian/p/7746158.html
 	//https://www.cnblogs.com/bobc/p/8809761.html
 	public static JSONObject doHttp(String method, Map<String, Object> params) throws IOException {
-		// 构建请求参数  
-        StringBuffer paramsSB = new StringBuffer();
-		if (params != null) {  
-            for (Entry<String, Object> e : params.entrySet()) {
-            	paramsSB.append(e.getKey());  
-            	paramsSB.append("=");  
-            	paramsSB.append(e.getValue());  
-            	paramsSB.append("&");  
-            }  
-            paramsSB.substring(0, paramsSB.length() - 1);  
-        }  
-		
-		StringBuffer sbf = new StringBuffer(); 
-		String strRead = null; 
-		URL url = new URL(SERVICE_URL+method);
-		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-		connection.setRequestMethod("POST");//请求post方式
-		connection.setDoInput(true); 
-		connection.setDoOutput(true); 
-		//header内的的参数在这里set    
-		//connection.setRequestProperty("key", "value");
-		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-		connection.connect(); 
-		
-		OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(),"UTF-8"); 
-		//OutputStream writer = connection.getOutputStream(); 
-		writer.write(paramsSB.toString());
-		writer.flush();
-		InputStream is = connection.getInputStream(); 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-		while ((strRead = reader.readLine()) != null) {
-			sbf.append(strRead); 
-			sbf.append("\r\n"); 
+		JSONObject resultJO = null;
+		try {
+			// 构建请求参数  
+			StringBuffer paramsSB = new StringBuffer();
+			if (params != null) {  
+			    for (Entry<String, Object> e : params.entrySet()) {
+			    	paramsSB.append(e.getKey());  
+			    	paramsSB.append("=");  
+			    	paramsSB.append(e.getValue());  
+			    	paramsSB.append("&");  
+			    }  
+			    paramsSB.substring(0, paramsSB.length() - 1);
+			}  
+			
+			StringBuffer sbf = new StringBuffer(); 
+			String strRead = null; 
+			URL url = new URL(SERVICE_URL+method);
+			HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("POST");//请求post方式
+			connection.setDoInput(true); 
+			connection.setDoOutput(true); 
+			//header内的的参数在这里set    
+			//connection.setRequestProperty("key", "value");
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			connection.connect(); 
+			
+			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(),"UTF-8"); 
+			//OutputStream writer = connection.getOutputStream(); 
+			writer.write(paramsSB.toString());
+			writer.flush();
+			InputStream is = connection.getInputStream(); 
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			while ((strRead = reader.readLine()) != null) {
+				sbf.append(strRead); 
+				sbf.append("\r\n"); 
+			}
+			reader.close();
+			
+			connection.disconnect();
+			String result = sbf.toString();
+			System.out.println("result==="+result);
+			resultJO = new JSONObject(result);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			resultJO = new JSONObject();
+			resultJO.put("success", "false");
 		}
-		reader.close();
-		
-		connection.disconnect();
-		String result = sbf.toString();
-		System.out.println("result==="+result);
-		JSONObject resultJO = new JSONObject(result);
 		return resultJO;
 	}
 
@@ -75,6 +87,9 @@ public class APIUtil {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			if("Connection refused: connect".equals(e.getMessage())) {
+				resultJO.put("success", "false");
+			}
 		}
 		finally {
 			return resultJO;
